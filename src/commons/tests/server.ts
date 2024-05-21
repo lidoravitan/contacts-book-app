a00708e1370622a9a0213fa8ac31d436fe65b1e9
Lidor Avitan <lidor.inc@gmail.com>
2024-05-21 12:58:39 +0300

    chore: fix uuid

--- src/commons/tests/server.ts
+++ src/commons/tests/server.ts
@@ -2,11 +2,12 @@ import { HttpResponse, http } from 'msw'
 import { setupServer } from 'msw/node'
 import { appConfig } from '../appConfig'
 import { Contact } from '../types'
+import { v4 as uuid } from 'uuid'
 
 export const handlers = [
   http.get(`${appConfig.BASE_URL}/contacts`, () => {
     return HttpResponse.json<Contact[]>([
-      { id: '1', name: 'Jhon', email: 'email-address', phone: 'phone-number', address: 'home-address' },
+      { id: uuid(), name: 'Jhon', email: 'email-address', phone: 'phone-number', address: 'home-address' },
     ])
   }),
 ]
